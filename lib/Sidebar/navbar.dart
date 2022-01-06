3d92a61808de308d6e3abd37deb07daaf21dd971
Januyan <janu1711@gmail.com>
2022-01-06 20:45:02 +0530

    navigation bar complete

--- lib/Sidebar/navbar.dart
+++ lib/Sidebar/navbar.dart
@@ -10,37 +10,68 @@ class NavBar extends StatefulWidget {
 class _NavState extends State<NavBar> {
   @override
   Widget build(BuildContext context) {
-    return Drawer(
-      child: ListView(
-        padding: EdgeInsets.zero,
-        children: [
-          UserAccountsDrawerHeader(
-            accountName: Text('Seralagan Januyan'),
-            accountEmail: Text('januyan1711@gmail.com'),
-            currentAccountPicture: CircleAvatar(
-              child: ClipOval(
-                child: Image.asset(
-                  "assets/my.jpg",
-                  width: 90,
-                  height: 90,
-                  fit: BoxFit.cover,
+    return Container(
+      child: Drawer(
+        child: ListView(
+          padding: EdgeInsets.zero,
+          children: [
+            UserAccountsDrawerHeader(
+              accountName: Text('Seralagan Januyan'),
+              accountEmail: Text('januyan1711@gmail.com'),
+              currentAccountPicture: CircleAvatar(
+                child: ClipOval(
+                  child: Image.asset(
+                    "assets/my.jpg",
+                    width: 90,
+                    height: 90,
+                    fit: BoxFit.cover,
+                  ),
                 ),
               ),
+              decoration: BoxDecoration(
+                  color: Colors.blue,
+                  image: DecorationImage(
+                      image: AssetImage("assets/bg.jpg"), fit: BoxFit.cover)),
             ),
-            decoration: BoxDecoration(
-                color: Colors.blue,
-                image: DecorationImage(
-                    image: AssetImage("assets/bg.jpg"), fit: BoxFit.cover)),
-          )
-
-          // ListTile (
-
-          //   leading: Icon(Icons.favorite),
-          //   title:Text('Favorites') ,
-          //   onTap: () => null,
-
-          // ),
-        ],
+            ListTile(
+              leading: Icon(Icons.favorite),
+              title: Text('Favorites'),
+              onTap: () => null,
+            ),
+            ListTile(
+              leading: Icon(Icons.people),
+              title: Text('Friends'),
+              onTap: () => null,
+            ),
+            ListTile(
+              leading: Icon(Icons.share),
+              title: Text('Share'),
+              onTap: () => null,
+            ),
+            ListTile(
+              leading: Icon(Icons.notifications),
+              title: Text('Request'),
+              onTap: () => null,
+            ),
+            Divider(),
+            ListTile(
+              leading: Icon(Icons.settings),
+              title: Text('Settings'),
+              onTap: () => null,
+            ),
+            ListTile(
+              leading: Icon(Icons.description),
+              title: Text('Policies'),
+              onTap: () => null,
+            ),
+            Divider(),
+            ListTile(
+              leading: Icon(Icons.logout),
+              title: Text('Logout'),
+              onTap: () => null,
+            ),
+          ],
+        ),
       ),
     );
   }
